cc8a958741d180582155d6402b4f47cb584efc2d
Olibia Tsati <olibia@hardpixel.eu>
2017-10-17 18:24:59 +0300

    require files

--- lib/active_seo.rb
+++ lib/active_seo.rb
@@ -1,4 +1,8 @@
-require "active_seo/version"
+require 'active_support'
+require 'active_delegate'
+require 'active_seo/meta'
+require 'active_seo/models/seo_metum'
+require 'active_seo/version'
 
 module ActiveSeo
   # Your code goes here...
