48e59701d613f439861c3c7a42e7bcd1e6d4e563
Olibia Tsati <olibia@hardpixel.eu>
2017-10-18 22:31:29 +0300

    separate configs

--- lib/active_seo.rb
+++ lib/active_seo.rb
@@ -9,20 +9,34 @@ require 'active_seo/version'
 module ActiveSeo
   # Config class
   class Config < Hashie::Dash
-    property :title_limit
-    property :description_limit
-    property :keywords_limit
-    property :keywords_separator
+    property :title_limit,          default: 70
+    property :description_limit,    default: 160
+    property :keywords_limit,       default: 255
+    property :title_fallback,       default: true
+    property :description_fallback, default: true
+    property :generate_keywords,    default: true
   end
 
   # Set attr accessors
-  mattr_accessor :config
+  mattr_accessor :config, :opengraph_config, :twitter_config
 
   # Set config options
-  @@config = Config.new
+  @@config           = Config.new
+  @@opengraph_config = Hashie::Mash.new
+  @@twitter_config   = Hashie::Mash.new
 
-  # Setup module
+  # Setup module config
   def self.setup
     yield config
+  end
+
+  # Setup module opengraph config
+  def self.opengraph_setup
+    yield opengraph_config
+  end
+
+  # Setup module opengraph config
+  def self.twitter_setup
+    yield twitter_config
   end
 end
--- lib/generators/active_seo/templates/initializer/initializer.rb
+++ lib/generators/active_seo/templates/initializer/initializer.rb
@@ -1,6 +1,17 @@
 ActiveSeo.setup do |config|
-  config.title_limit        = 70
-  config.description_limit  = 160
-  config.keywords_limit     = 255
-  config.keywords_separator = ', '
+  # config.title_limit          = 70
+  # config.description_limit    = 160
+  # config.keywords_limit       = 255
+  # config.keywords_separator   = ', '
+  # config.title_fallback       = true
+  # config.description_fallback = true
+  # config.generate_keywords    = true
+end
+
+ActiveSeo.opengraph_setup do |config|
+  # config.type = 'website'
+end
+
+ActiveSeo.twitter_setup do |config|
+  # config.card = 'summary'
 end
