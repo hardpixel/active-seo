31c5ddfd21a8da53b60412efbf8dd1d1c3a1f603
Jonian Guveli <jonian@hardpixel.eu>
2017-11-13 12:53:17 +0200

    move config outside main module

--- lib/active_seo.rb
+++ lib/active_seo.rb
@@ -11,28 +11,7 @@ require 'active_seo/seo_meta'
 require 'active_seo/version'
 
 module ActiveSeo
-  # Config class
-  class Config < Hashie::Dash
-    # Set properties and defaults
-    property :title_limit,          default: 70
-    property :description_limit,    default: 160
-    property :keywords_limit,       default: 255
-    property :title_fallback,       default: true
-    property :description_fallback, default: true
-    property :generate_keywords,    default: true
-    property :opengraph,            default: {}
-    property :twitter,              default: {}
 
-    def opengraph_setup(&block)
-      self.opengraph = Hashie::Mash.new
-      yield self.opengraph
-    end
-
-    def twitter_setup(&block)
-      self.twitter = Hashie::Mash.new
-      yield self.twitter
-    end
-  end
 
   # Set attr accessors
   mattr_accessor :config
