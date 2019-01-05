00d6a6e733ccf9f6e074c06104b563cb156fb381
Jonian Guveli <jonian@hardpixel.eu>
2019-01-05 13:01:37 +0200

    disable hashie warnings on og and twitter configs

--- lib/active_seo/config.rb
+++ lib/active_seo/config.rb
@@ -1,4 +1,12 @@
 module ActiveSeo
+  class OpenGraphConfig < Hashie::Mash
+    self.disable_warnings
+  end
+
+  class TwitterCardConfig < Hashie::Mash
+    self.disable_warnings
+  end
+
   class Config < Hashie::Dash
     # Set properties and defaults
     property :class_name,           default: 'ActiveSeo::SeoMetum'
@@ -13,12 +21,12 @@ module ActiveSeo
     property :twitter,              default: {}
 
     def opengraph_setup(&block)
-      self.opengraph = Hashie::Mash.new
+      self.opengraph = OpenGraphConfig.new
       yield self.opengraph
     end
 
     def twitter_setup(&block)
-      self.twitter = Hashie::Mash.new
+      self.twitter = TwitterCardConfig.new
       yield self.twitter
     end
   end
